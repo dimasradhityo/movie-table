5066feb9f7d56d9d9f228cd42c3d7eb84e1cb364
dimasradhityo <dimasradhityo@gmail.com>
2021-08-01 14:37:57 +0700

    new changed setupProxy http-proxy-middleware

--- server.js
+++ server.js
@@ -12,44 +12,44 @@
 //   console.log('CORS-enabled web server listening on port 80')
 // })
 
-const express = require('express')
-const request = require('request')
-const bodyParser = require('body-parser')
-const app = express();
+// const express = require('express')
+// const request = require('request')
+// const bodyParser = require('body-parser')
+// const app = express();
 
-const myLimit = typeof(process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
-console.log('Using limit: ', myLimit);
+// const myLimit = typeof(process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
+// console.log('Using limit: ', myLimit);
 
-app.use(bodyParser.json({limit: myLimit}));
+// app.use(bodyParser.json({limit: myLimit}));
 
-app.all('*', function (req, res, next) {
+// app.all('*', function (req, res, next) {
 
-    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+//     // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+//     res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
 
-    if (req.method === 'OPTIONS') {
-        // CORS Preflight
-        res.send();
-    } else {
-        var targetURL = req.header('https://n7b67.sse.codesandbox.io'); // Target-URL ie. https://example.com or http://example.com
-        if (!targetURL) {
-            res.send(500, { error: 'There is no Target-Endpoint header in the request' });
-            return;
-        }
-        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
-            function (error, response, body) {
-                if (error) {
-                    console.error('error: ' + response.statusCode)
-                }
-//                console.log(body);
-            }).pipe(res);
-    }
-});
+//     if (req.method === 'OPTIONS') {
+//         // CORS Preflight
+//         res.send();
+//     } else {
+//         var targetURL = req.header('https://n7b67.sse.codesandbox.io'); // Target-URL ie. https://example.com or http://example.com
+//         if (!targetURL) {
+//             res.send(500, { error: 'There is no Target-Endpoint header in the request' });
+//             return;
+//         }
+//         request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
+//             function (error, response, body) {
+//                 if (error) {
+//                     console.error('error: ' + response.statusCode)
+//                 }
+// //                console.log(body);
+//             }).pipe(res);
+//     }
+// });
 
-app.set('port', process.env.PORT || 3001);
+// app.set('port', process.env.PORT || 3001);
 
-app.listen(app.get('port'), function () {
-    console.log('Proxy server listening on port ' + app.get('port'));
-});
+// app.listen(app.get('port'), function () {
+//     console.log('Proxy server listening on port ' + app.get('port'));
+// });
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,13 +1,39 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const express = require('express')
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
+// const app = express()
+// //CONFIG
+// const PORT = 3001
+// const HOST = 'localhost'
+// const API_SERVICE_URL = 'https://n7b67.sse.codesandbox.io' 
+
+// app.use('/graphql', createProxyMiddleware({
+//     target:API_SERVICE_URL,
+//     changeOrigin: true,
+//     pathRewrite: {
+//         ['^/graphql']: '',
+//     }
+// }))
+
+// app.listen(PORT, HOST, () =>{
+//     console.log(`starting dev Proxy at ${HOST}:${PORT}`)
+// })
+
+// module.exports = app
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
     '/graphql',
     createProxyMiddleware({
       target: 'https://n7b67.sse.codesandbox.io',
       changeOrigin: true,
+      
+
     })
   );
 };
 
 //"proxy": "https://n7b67.sse.codesandbox.io",
+
+// "http-proxy-middleware": "^2.0.1",
